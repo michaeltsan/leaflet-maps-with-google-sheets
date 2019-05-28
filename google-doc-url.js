3871e5bcbaa053c53dafce0cef0be2d0a022ea99
michaeltsan <39771578+michaeltsan@users.noreply.github.com>
2019-05-28 15:48:41 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1tCkpdfO_sbVexvn3OyDu2NRwj5yrc9WfKasg-adu0K4/edit#gid=0';
